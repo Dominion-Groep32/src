6f15185c87e25ef98b8ab49f6ef41020c174b3be
LaraVantorreVerlinde <laraavt@hotmail.com>
2016-03-22 16:13:33 +0100

    Aanpassing van Lara
    Finally en aanhalingsteken

--- JavaCode/Test.java
+++ JavaCode/Test.java
@@ -4,8 +4,8 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Het werkt! Joepie Finaly");
-		System.out.println("Hallo Maxim);
+		System.out.println("Het werkt! Joepie Finally");
+		System.out.println("Hallo Maxim");
 		
 	}
 
