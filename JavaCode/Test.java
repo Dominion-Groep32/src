8dcdbd4912eb63770c5471bd1b7585d75c4ab59d
GrietCoysman <griet.coysman@student.howest.be>
2016-03-22 15:46:41 +0100

    Aanpassing Griet

--- JavaCode/Test.java
+++ JavaCode/Test.java
@@ -5,6 +5,7 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Het werkt! Joepie Finaly");
+		System.out.println("Hallo Maxim);
 		
 	}
 
