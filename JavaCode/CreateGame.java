ed90d0152180d38d24709d59f98ac7513246430d
MaximTernier <maxim.ternier@student.howest.be>
2016-03-23 16:16:06 +0100

    GitHub Fixes

--- JavaCode/CreateGame.java
+++ JavaCode/CreateGame.java
@@ -7,33 +7,16 @@ public class CreateGame {
 	public static void main(String[] args) {
 				
 		// TODO Auto-generated method stub
-<<<<<<< HEAD
-		
-		/*
-		GUI gui = new GUI();
-		gui.GenerateCardsField();
-		gui.GenerateCardsHand();
- 		*/
-		
-		 
-		
-				
-		
-	
-=======
 
 		// GUI gui = new GUI();
-		 //gui.GenerateCardsField();
-		   //gui.GenerateCardsHand();
+		//gui.GenerateCardsField();
+		//gui.GenerateCardsHand();
 		  
 		  GameActions test = new GameActions();
 		  test.basicTurn(); // zorgt ervoor dat je 1 actie / 1 buy / 0 gp's hebt 
 		  
-		  
 		  System.out.println("aantal coins is "+test.getCoins());
 		  
-		  
->>>>>>> c77f6c25ff314395c9b3947d482128881f145853
 	}
 
 }
