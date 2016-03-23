a6c8ed10d09387a404560d22a4a7ba756c46a704
JensPauwels <jens.pauwels@student.howest.be>
2016-03-23 16:10:35 +0100

    THE BOMB IS REAL

--- JavaCode/CreateGame.java
+++ JavaCode/CreateGame.java
@@ -6,13 +6,11 @@ public class CreateGame {
 		// TODO Auto-generated method stub
 
 		// GUI gui = new GUI();
-		// gui.GenerateCardsField();
-		  // gui.GenerateCardsHand();
+		 //gui.GenerateCardsField();
+		   //gui.GenerateCardsHand();
 		  
 		  GameActions test = new GameActions();
-		  test.basicTurn();
-		  test.copperAction();
-		  test.copperAction();
+		  test.basicTurn(); // zorgt ervoor dat je 1 actie / 1 buy / 0 gp's hebt 
 		  
 		  
 		  System.out.println("aantal coins is "+test.getCoins());
