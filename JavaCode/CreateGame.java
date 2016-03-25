b79f28158a63de3cff01658813c95cebf713b35b
GrietCoysman <griet.coysman@student.howest.be>
2016-03-25 13:16:08 +0100

    Griet

--- JavaCode/CreateGame.java
+++ JavaCode/CreateGame.java
@@ -1,19 +1,9 @@
 package JavaCode;
 
 
-import java.awt.Button;
-import java.awt.Desktop.Action;
+
 import java.util.LinkedList;
-import java.util.Random;
-import java.util.concurrent.LinkedBlockingDeque;
 
-import javax.smartcardio.Card;
-import javax.swing.ImageIcon;
-
-import org.omg.CORBA.PUBLIC_MEMBER;
-
-import com.sun.scenario.effect.DisplacementMap;
-import com.sun.xml.internal.bind.v2.schemagen.xmlschema.List;
 
 
 public class CreateGame {
@@ -27,14 +17,14 @@ public class CreateGame {
 		//gui.GenerateCardsHand();
 		
 		
-		GameActions action = new GameActions();
+		DeckActions action = new DeckActions();
 		CardDetails card = new CardDetails();
 		
 		
-		 
+		card.startUp();
+		
 		System.out.println("--------------------------Standaard------------------------------------------");
 		
-		card.startUp();
 		LinkedList<String> startDeck = action.startDeckCards();
 		action.displayDeck(startDeck);
 
@@ -44,17 +34,17 @@ public class CreateGame {
 		action.displayDeck(playableDeck);
 		
 
-		System.out.println("--------------------------------------deck 5-------------------------------------------");
+		System.out.println("--------------------------------------drawHand-------------------------------------------");
 		
 		LinkedList<String> drawHand = action.drawHand(playableDeck);
 		action.displayDeck(drawHand);
 
-		System.out.println("--------------------------playable -5------------------------------------------");
+		System.out.println("--------------------------playableDeck------------------------------------------");
 		
-		playableDeck = action.decreasePlayAbleDeck(playableDeck);
+		playableDeck = action.decreasePlayableDeck(playableDeck,5);
 		action.displayDeck(playableDeck);
 		
-		System.out.println("--------------------------add witch print discardDeck------------------------------------------");
+		System.out.println("--------------------------add actioncard to discardDeck------------------------------------------");
 		
 		card.witch();
 		LinkedList<String> startDiscardDeck = new LinkedList<String>();
@@ -66,35 +56,14 @@ public class CreateGame {
 		discardDeck = action.mergeLists(discardDeck, drawHand);
 		action.displayDeck(discardDeck);
 	
-		System.out.println("--------------------nieuwe playabledeck-------------------------------------------");
+		System.out.println("--------------------Controle playabledeck-------------------------------------------");
 		
 		playableDeck=action.newPlayableDeck(playableDeck, discardDeck);
 		action.displayDeck(playableDeck);
 		
-		System.out.println("-------------------------------shuffle--------------------------------------------");
-		
-		playableDeck = action.shuffle(playableDeck); 
-		action.displayDeck(playableDeck);
-		
-		System.out.println("--------------------------------------deck 5-------------------------------------------");
+		System.out.println("--------------------------------------drawHand-------------------------------------------");
 		
 		drawHand = action.drawHand(playableDeck);
 		action.displayDeck(drawHand);
-		
-		System.out.println("--------------------------playableDeck------------------------------------------");
-		
-		action.displayDeck(playableDeck);
-		
-		System.out.println("--------------------------playable -5------------------------------------------");
-		
-		playableDeck = action.decreasePlayAbleDeck(playableDeck);
-		action.displayDeck(playableDeck);
-		
-		System.out.println("--------------------------add adventure print discardDeck------------------------------------------");
-		
-		card.adventurer();
-		discardDeck = action.addCardToList(discardDeck, card.cardName);
-		action.displayDeck(discardDeck);
-		
 	}
 }
--- JavaCode/GameActions.java
+++ JavaCode/GameActions.java
@@ -1,83 +1,8 @@
 package JavaCode;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.LinkedList;
-
-import org.omg.CORBA.PUBLIC_MEMBER;
-
-import sun.awt.image.ImageWatched.Link;
-
 public class GameActions {
 	
-	public LinkedList<String> discardedCardList = new LinkedList<String>();
-	public LinkedList<String> CardsInHand = new LinkedList<String>();
-	
-	public LinkedList startDeckCards(){
-		
-		LinkedList<String> list = new LinkedList<String>();
-	 
-		for (int i = 0; i < 5; i++) {list.add("copper");}
-		for (int j = 0; j < 3; j++) {list.add("estate");}
-		return list;
-	}	
-	
-	public void displayDeck(LinkedList<String> list){
-	
-		for(String x : list) System.out.println(x);
-		}
-	
-	public LinkedList shuffle(LinkedList<String> list){
-		Collections.shuffle(list);
-		return list;
-	}
-	
-	public LinkedList addCardToList(LinkedList<String> list, String card){
-		list.add(card);
-		return list;
-	}
-	
-	public LinkedList drawHand(LinkedList<String> list){
-		CardsInHand.clear();
-		for (int i = 0; i < 5; i++) {CardsInHand.add(list.get(i));}
-		return CardsInHand;
-	}
-	
-	public LinkedList discardDeck(LinkedList<String> list) {
-		
-		for (int i = 0; i < list.size(); i++) {discardedCardList.add(list.get(i));}
-		return discardedCardList;
-	}
-	
-	public LinkedList<String> mergeLists(LinkedList<String> mainList, LinkedList<String> addToAnotherList ){
-		
-		for (int i = 0; i < addToAnotherList.size(); i++) {mainList.add(addToAnotherList.get(i));}
-		return mainList;
-	}
-	
-	public LinkedList<String> decreasePlayAbleDeck(LinkedList<String> list){
-		
-		for (int i = 0; i < 5; i++) {list.removeFirst();}
-		return list;
-	}
-	
-	private int getListSize(LinkedList<String> list){
-		return list.size();
-	}
 	
 	
-	public LinkedList<String> newPlayableDeck(LinkedList<String> playableList, LinkedList<String> discardList){
-		
-		if(getListSize(playableList)<5){
-			
-			LinkedList<String> newPlayableList = mergeLists(playableList, discardList);
-			return newPlayableList;
-		}
-		else{return playableList;}
-	}
-	
-	
-	
-	
-	
-	}
+
+}
