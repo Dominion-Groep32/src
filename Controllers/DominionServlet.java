d131c3fbfc35654f77e81601cd6a90eefc07a447
MaximTernier <maxim.ternier@student.howest.be>
2016-05-17 12:58:55 +0200

    yes :)

--- Controllers/DominionServlet.java
+++ Controllers/DominionServlet.java
@@ -6,55 +6,88 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-//import org.json.*;
+import org.json.*;
 import engine.*;
 
 /**
  * Servlet implementation class DominionServlet
  */
-//public class DominionServlet extends HttpServlet {
-	//private static final long serialVersionUID = 1L;
+
+public class DominionServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
 	
-	//private GameEngine engine;
+	private GameEngine engine;	// FIXME: zou via getServletContext().get/setAttribute moeten werken
+	private Speler speler;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-	  /*
+	
     public DominionServlet() {
         super();
+        engine = null;
         // TODO Auto-generated constructor stub
     }
   
-    private void addPlayer(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
-    {	
-		String spelerNaam = request.getParameter("spelerNaam");
-		Speler gebruiker = new Speler(spelerNaam);
+    private void spelerToevoegen(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {	
+    	// this is the call that initiates a NEWLY begun game
+    	engine = new GameEngine();
+    	
+    	String spelerNaam = request.getParameter("spelerNaam");
+    	String spelerNaam2 = request.getParameter("spelerNaam2");
+    	
+    	String spelers[] = new String[2];
+    	spelers[0] = spelerNaam;
+    	spelers[1] = spelerNaam2;
+    	
+		engine.maakSpelersAan(spelers);
 		
 		//JSONObject jsonObj = new JSONObject();
-		jsonObj.put("speler", gebruiker.geefNaam());
-		
-		response.getWriter().write(jsonObj.toString());
+		//Speler speler = new Speler(spelerNaam);
+    	//jsonObj.put("speler", speler.geefNaam());
     }
+		
+    private void kaartenInHand(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    	JSONObject jsonObj = new JSONObject();
+    	//jsonObj.put("Huidige kaarten in hand", engine.kaartInHand());
+    	response.getWriter().write(jsonObj.toString());
+	}
+    
+    private void koperKopen(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    	JSONObject jsonObj = new JSONObject();    	
+    	response.getWriter().write(jsonObj.toString());
+	}
+    
+    private void huidigeKaarten(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    	speler.kaartenInHand();
+	}
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-    /*
+    
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.setHeader("Access-Control-Allow-Origin", "*");
 		
 		switch(request.getParameter("operation"))
 		{
-		case "addPlayer":
-			addPlayer(request, response);
+		case "spelerToevoegen":
+			spelerToevoegen(request, response);
 			break;
 			
-		case "ietsanders":
+		case "kaartenInHand":
+			kaartenInHand(request, response);
 			break;
+			
+		case "koperKopen":
+			koperKopen(request, response);
+			break;
+			
+		case "huidigeKaarten":
+			huidigeKaarten(request, response);
 					
 		default:
-			// ErrorMsg(request, response);
+			//ErrorMsg(request, response);
 			break;
 		}	
 	}
@@ -62,7 +95,7 @@ import engine.*;
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-    /*
+    
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
@@ -70,4 +103,3 @@ import engine.*;
 		response.getWriter().append("hello world vanuit post");
 	}
 }
-*/
