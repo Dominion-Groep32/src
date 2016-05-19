7f3474a29d963cc682db21350fcb5971c5242c93
JensPauwels <jens.pauwels@student.howest.be>
2016-05-19 17:45:40 +0200

    engine fix

--- Controllers/DominionServlet.java
+++ Controllers/DominionServlet.java
@@ -6,46 +6,45 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 import org.json.*;
 import engine.*;
 
 public class DominionServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	private SpelFuncties engine = new SpelFuncties();
-	private Speler huidigeSpeler;
+	//private Speler huidigeSpeler;
 	
 	
     public DominionServlet() {
         super();
     }
   
-    private void spelersToevoegen(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {	
+    private void spelersToevoegen(HttpServletRequest request, HttpServletResponse response, SpelFuncties engine) throws ServletException, IOException {	
     	JSONObject jsonObj = new JSONObject();
     	String spelerNaam = request.getParameter("speler1");
     	String spelerNaam2 = request.getParameter("speler2");
     	
     	String spelers[] = {spelerNaam, spelerNaam2};
     	engine.maakSpelersAan(spelers);
-    	this.huidigeSpeler = engine.geefHuidigeSpeler();
+    	//this.huidigeSpeler = engine.geefHuidigeSpeler();
 		jsonObj.put("Speler1", spelers[0]);
 		jsonObj.put("Speler2", spelers[1]);
 		response.getWriter().write(jsonObj.toString());
 		
     }
 	
-    private void geefKaartenInHandVanDeHuidigeSpeler(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    	
+    private void geefKaartenInHandVanDeHuidigeSpeler(HttpServletRequest request, HttpServletResponse response, SpelFuncties engine) throws ServletException, IOException {
     	JSONArray arrayObj = new JSONArray();
-    	
     	engine.trekKaart(engine.geefHuidigeSpeler().trekStapel(), 5);
     	
 		for(int i=0; i<engine.geefHuidigeSpeler().kaartenInHand().size();i++){
 			arrayObj.put(i, engine.geefHuidigeSpeler().kaartenInHand().get(i).naam());
 		}
+		
 		response.getWriter().write(arrayObj.toString());
     }
 
-    private void genereerActieKaart(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    private void genereerActieKaart(HttpServletRequest request, HttpServletResponse response, SpelFuncties engine) throws ServletException, IOException {
     	
     	JSONArray arrayObj = new JSONArray();
     	List<Kaart> actieKaarten = engine.actiekaartenGenereren();
@@ -54,27 +53,39 @@ public class DominionServlet extends HttpServlet {
 		}
 		response.getWriter().write(arrayObj.toString());
     }
+   
+    
+    
     
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.setHeader("Access-Control-Allow-Origin", "*");
-		// FIXME: zou via getServletContext().get/setAttribute moeten werken
-		//get servlet context
+		//response.setContentType("application/json");
 		
-		if(engine == null){
-			engine = new SpelFuncties();
-			 //set servlet context
+		SpelFuncties gameEngine = (SpelFuncties) request.getServletContext().getAttribute("SpelFuncties");
+		if(gameEngine == null)
+		{
+			gameEngine = new SpelFuncties();
+			request.getServletContext().setAttribute("SpelFuncties", gameEngine);
+			
 		}
+		
 		switch(request.getParameter("operation"))
 		{
 		case "spelerToevoegen":
-			spelersToevoegen(request, response);
+			spelersToevoegen(request, response, gameEngine);
 			break;
 			
-		case "huidigeSpeler":
-			geefKaartenInHandVanDeHuidigeSpeler(request, response);
+		case "geefKaartenInHand":
+			geefKaartenInHandVanDeHuidigeSpeler(request, response, gameEngine);
 			break;
 		case "actieKaartenGeneren":
-			genereerActieKaart(request, response);
+			genereerActieKaart(request, response, gameEngine);
+			break;
+		case "stopBeurt":
+			
+			gameEngine.volgendeSpeler();
+			genereerActieKaart(request, response, gameEngine);
+			
 			break;
 			
 		
